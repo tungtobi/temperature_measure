9d65782b6de0dbb61362abfc1f33b022f81decbe
tungtobi <tungtobi@gmail.com>
2020-10-27 16:25:50 +0700

    refactor json keys

--- Models/TemperatureModel.go
+++ Models/TemperatureModel.go
@@ -4,8 +4,8 @@ import "time"
 
 type Temperature struct {
 	ID         uint    `gorm:"primaryKey"`
-	Enviroment float64 `json:"enviroment" gorm:"enviroment"`
-	Target     float64 `json:"target" gorm:"target"`
+	Env float64 `json:"env" gorm:"enviroment"`
+	Obj     float64 `json:"obj" gorm:"target"`
 	CreatedAt  time.Time
 }
 
